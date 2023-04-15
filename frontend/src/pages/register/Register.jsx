4b7eba81aa818e1828e6288ed784ecd70c9da54a
yosef-w <ywolday24@gmail.com>
2023-04-15 14:51:43 -0500

    Add signup page

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import ""
+import { FaFacebook, FaTwitter, FaGithub } from "react-icons/fa"
+import { FcGoogle } from "react-icons/fc"
+import "./register.css"
 
 export default function Register({ flashMessage }) {
 
@@ -45,61 +47,72 @@ export default function Register({ flashMessage }) {
 
     return (
         <>
-                <title>Sign Up</title>
-        <div className="container">
-            <div className="row">
-            <div
-                className="col-sm-12 col-md-4 d-flex flex-column justify-content-center align-items-center"
-                id="content"
-            >
-                <h1 className="display-2 mb-5">Sign Up</h1>
-                <div className="form-floating">
-                <i className="icon fa-regular fa-user" />
-                <input
-                    type="text"
-                    name=""
-                    id="user"
-                    placeholder="Username"
-                    className="form-control"
-                />
-                <label htmlFor="user">Username</label>
-                </div>
-                <div className="form-floating">
-                <i className="icon fa-regular fa-envelope" />
-                <input
-                    type="text"
-                    name=""
-                    id="email"
-                    placeholder="Email"
-                    className="form-control"
-                />
-                <label htmlFor="email">Email</label>
-                </div>
-                <div className="form-floating">
-                <i className="icon fa-solid fa-lock" />
-                <input
-                    type="text"
-                    name=""
-                    id="password"
-                    placeholder="Password"
-                    className="form-control"
-                />
-                <label htmlFor="password">Password</label>
-                </div>
-                <button>Sign Up</button>
-                <p>Or Sign Up With Social Platforms</p>
-                <div className="w-75 d-flex justify-content-between mt-4">
-                <i className="fa-brands fa-facebook fa-2xl" />
-                <i className="fa-brands fa-twitter fa-2xl" />
-                <i className="fa-brands fa-google fa-2xl" />
-                <i className="fa-brands fa-linkedin-in fa-2xl" />
+            <title>Sign Up</title>
+            <div className="container">
+                <div className="row">
+                    <div
+                        className="col-sm-12 col-md-6 d-flex flex-column justify-content-center align-items-center"
+                        id="content"
+                    >
+                        <h1 className="display-2 mb-5">Sign Up</h1>
+                        <div className="form-floating">
+                            <i className="icon fa-regular fa-user" />
+                            <input
+                                type="text"
+                                name=""
+                                id="user"
+                                placeholder="Username"
+                                className="form-control"
+                            />
+                            <label htmlFor="user">Username</label>
+                        </div>
+                        <div className="form-floating">
+                            <i className="icon fa-regular fa-envelope" />
+                            <input
+                                type="text"
+                                name=""
+                                id="email"
+                                placeholder="Email"
+                                className="form-control"
+                            />
+                            <label htmlFor="email">Email</label>
+                        </div>
+                        <div className="form-floating">
+                            <i className="icon fa-solid fa-lock" />
+                            <input
+                                type="text"
+                                name=""
+                                id="password"
+                                placeholder="Password"
+                                className="form-control"
+                            />
+                            <label htmlFor="password">Password</label>
+                        </div>
+                        <div className="form-floating">
+                            <i className="icon fa-solid fa-lock" />
+                            <input
+                                type="text"
+                                name=""
+                                id="password"
+                                placeholder="Password"
+                                className="form-control"
+                            />
+                            <label htmlFor="password">Confirm Password</label>
+                        </div>
+                        <button>Sign Up</button>
+                        <p>Or Sign Up With Social Platforms</p>
+                        <div className="w-75 d-flex justify-content-between mt-4">
+                            <FaFacebook size={30}/>
+                            <FaTwitter size={30}/>
+                            <FcGoogle size={30}/>
+                            <FaGithub size={30}/>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-6">
+                        <img src="/assets/Elegant and Minimalist Medical Logo.png" alt="" />
+                    </div>
                 </div>
             </div>
-            <div className="col-sm-12 col-md-8">
-                <img src="assets/sign_up/Group 10.png" alt="" />
-            </div>
-            </div>
-        </div>
-  </>
+        </>
     )
-}
+};
