bc131e0b16fb7d77dd1a966115e4c61ef584b433
yosef-w <ywolday24@gmail.com>
2023-06-02 18:26:33 -0500

    Add new profile sidbar

---
+++ frontend/src/pages/profile/ProfileSidebar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className="col-12 col-md-3 p-0">
+            <ul className="nav navbar-show flex-column">
+                <h1 className="pt-4 pb-2 sh1">Organization</h1>
+                <li className="nav-item">
+                    <a className="nav-link" href="/#">
+                        <span className="d-flex jusitfy-content-center">
+                            <span className="icon icon-briefcase ms-1" />
+                            My Job Feed
+                        </span>
+                    </a>
+                </li>
+                <li className="nav-item active">
+                    <a className="nav-link" href="/#">
+                        <span className="d-flex jusitfy-content-center">
+                            <span className="icon icon-user ms-1" style={{ marginRight: 20 }} />
+                            Profile
+                        </span>
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/#">
+                        <span className="d-flex jusitfy-content-center">
+                            <span className="icon icon-dashboard2" />
+                            Dashboard
+                        </span>
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/#">
+                        <span className="d-flex jusitfy-content-center">
+                            <span className="icon icon-save2" />
+                            Saved Jobs
+                        </span>
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/#">
+                        <span className="d-flex jusitfy-content-center">
+                            <span className="icon icon-settings" />
+                            Setting
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    )
+}
+
+export default Sidebar
